ba5687d10eb99e3a9f1247338104dd6e0e493ede
Sunshine0213 <138002841+Sunshine0213@users.noreply.github.com>
2023-08-01 22:47:09 -0700

    fix Navbar

--- src/pages/_app.page.js
+++ src/pages/_app.page.js
@@ -27,7 +27,7 @@ const App = ({ Component, pageProps }) => {
   const [storedTheme] = useLocalStorage('theme', 'dark');
   const [state, dispatch] = useReducer(reducer, initialState);
   const { route, asPath } = useRouter();
-  const canonicalRoute = route === '/portfolio2/' ? '' : `${asPath}`;
+  const canonicalRoute = route === '/' ? '' : `${asPath}`;
   useFoucFix();
 
   useEffect(() => {
